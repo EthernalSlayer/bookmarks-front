74e457b15fdfae22b117bbb0e0a7a8ac3b8eb057
EthernalSlayer <guillaume.b.a@protonmail.com>
2021-04-30 14:39:22 +0200

    pre rendering

--- src/App.js
+++ src/App.js
@@ -5,22 +5,35 @@ import Picture from './component/Picture';
 import Video from './component/Video';
 import Tag from './component/Tag';
 import Navigation from './component/Navigation';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import {
+	PICTURE_API_REQUEST,
+	VIDEO_API_REQUEST,
+	TAG_API_REQUEST,
+} from './store/constants/action-types';
+
+store.dispatch({ type: PICTURE_API_REQUEST, payload: { page: 1 } });
+store.dispatch({ type: VIDEO_API_REQUEST, payload: { page: 1 } });
+store.dispatch({ type: TAG_API_REQUEST });
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<Navigation />
-			</header>
-			<Container
-				style={{
-					marginTop: '100px',
-				}}
-			>
-				<Tag />
-				<Video />
-				<Picture />
-			</Container>
+			<Provider store={store}>
+				<header className='App-header'>
+					<Navigation />
+				</header>
+				<Container
+					style={{
+						marginTop: '100px',
+					}}
+				>
+					<Tag />
+					<Video />
+					<Picture />
+				</Container>
+			</Provider>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store/index';
-import {
-	PICTURE_API_REQUEST,
-	VIDEO_API_REQUEST,
-	TAG_API_REQUEST,
-} from './store/constants/action-types';
 
-store.dispatch({ type: PICTURE_API_REQUEST, payload: { page: 1 } });
-store.dispatch({ type: VIDEO_API_REQUEST, payload: { page: 1 } });
-store.dispatch({ type: TAG_API_REQUEST });
-
-ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+	hydrate(<App />, rootElement);
+} else {
+	render(<App />, rootElement);
+}
